accf902fad976085d2ab04a30bb7083ea2c6d04b
afboi <gustavo.acosta.fernandez1703@gmail.com>
2023-12-04 12:06:02 -0600

    esthetic changes in the user profile

--- user-profile.php
+++ user-profile.php
@@ -1,53 +1,55 @@
-<?php 
-    include_once "./database.php";
-    session_start();
-    // session_destroy();
+<?php
+include_once "./database.php";
+session_start();
+// session_destroy();
 
-    if(isset($_SESSION)){
-        if($_SESSION["isLoggedIn"]){
-            $user = $database->select("tb_users","*",[
-                "id_user" => $_SESSION["id"]
+if (isset($_SESSION)) {
+    if ($_SESSION["isLoggedIn"]) {
+        $user = $database->select("tb_users", "*", [
+            "id_user" => $_SESSION["id"]
+        ]);
+
+        if (count($user) > 0) {
+            $orders = $database->select("tb_order", "*", [
+                "id_user" => $user[0]["id_user"]
             ]);
-    
-            if(count($user) > 0){
-                $orders = $database->select("tb_order","*",[
-                    "id_user" => $user[0]["id_user"]
+
+            if (count($orders) > 0) {
+                $items = $database->select("tb_order", [
+                    "[>]tb_order_details" => ["id_order" => "id_order"],
+                    "[>]tb_dishes" => ["tb_order_details.id_dish" => "id_dishes"],
+                    "[>]tb_users" => ["id_user" => "id_user"]
+                ], [
+                    "tb_order.id_order",
+                    // "tb_order.order_date",
+                    "tb_order.id_order_type",
+                    // "tb_order.total_price",
+                    "tb_order.id_user",
+                    "tb_order_details.qty",
+                    "tb_order_details.price",
+                    "tb_dishes.dish_name",
+                    "tb_dishes.dish_img"
+                ], [
+                    "tb_order.id_user" => $user[0]["id_user"]
                 ]);
-    
-                if(count($orders) > 0){
-                    $items = $database->select("tb_order",[
-                        "[>]tb_order_details"=>["id_order" => "id_order"],
-                        "[>]tb_dishes"=>["tb_order_details.id_dish" => "id_dishes"],
-                        "[>]tb_users"=>["id_user" => "id_user"]
-                    ],[
-                        "tb_order.id_order",
-                        // "tb_order.order_date",
-                        "tb_order.id_order_type",
-                        // "tb_order.total_price",
-                        "tb_order.id_user",
-                        "tb_order_details.qty",
-                        "tb_order_details.price",
-                        "tb_dishes.dish_name",
-                        "tb_dishes.dish_img"
-                    ],[
-                        "tb_order.id_user" => $user[0]["id_user"]
-                    ]);
-                }
-    
-                var_dump($orders);
             }
+
+            var_dump($orders);
         }
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/main.css">
     <title>User Profile</title>
 </head>
+
 <body>
     <main class="user-main">
         <div class="user-section">
@@ -56,56 +58,59 @@
                     <img class="user-img" src="./imgs/user-svgrepo-com.svg" alt="user icon">
                 </div>
                 <div class="user-content">
-                    <p class="user-title"><?php echo $user[0]["fullname"]?></p>
-                    <p><?php echo $user[0]["email"]?></p>
+                    <p class="user-title">
+                        <?php echo $user[0]["fullname"] ?>
+                    </p>
+                    <p>
+                        <?php echo $user[0]["email"] ?>
+                    </p>
                 </div>
-                <div>
-                    <button>logout</button>
-                </div>
+                <button>
+                    <img src="./imgs/exit-svgrepo-com.svg" alt="exit">
+                </button>
             </div>
         </div>
         <section class="user-history">
             <h2 class="section-title">User history</h2>
-            <?php 
-                if(count($orders) > 0){
-                    foreach($orders as $order){
-                        echo "<div class='history-item'>";
-                            echo "<div class='history-header'>";
-                                echo "<p>Date: ".$order["order_date"]."</p>";
-                                echo "<p>Total price: $".$order["total_price"]."</p>";
-                            echo "</div>";
+            <?php
+            if (count($orders) > 0) {
+                foreach ($orders as $order) {
+                    echo "<div class='history-item'>";
+                    echo "<div class='history-header'>";
+                    echo "<p>Date: " . $order["order_date"] . "</p>";
+                    echo "<p>Total price: $" . $order["total_price"] . "</p>";
+                    echo "</div>";
 
-                            foreach($items as $item){
-                                if($item["id_order"] == $order["id_order"]){
-                                    echo "<div class='history-content'>";
-                                        echo "<div class='history-img-container'>";
-                                            echo "<img class='history-img' src='./imgs/".$item["dish_img"]."' alt=''>";
-                                        echo "</div>";
-                                        echo "<div class='history-text'>";
-                                            echo "<div class='histoy-text-container'>";
-                                                echo "<p class='title-history-text'>Dish name</p>";
-                                                echo "<p>".$item["dish_name"]."</p>";
-                                            echo "</div>";
-                                            echo "<div class='histoy-text-container'>";
-                                                echo "<p class='title-history-text'>Quantity</p>";
-                                                echo "<p>".$item["qty"]."</p>";
-                                            echo "</div>";
-                                            echo "<div class='histoy-text-container'>";
-                                                echo "<p class='title-history-text'>Price</p>";
-                                                echo "<p>$".$item["price"]."</p>";
-                                            echo "</div>";
-                                        echo "</div>";
-                                    echo "</div>";
-                                }
-                            }
-                        echo "</div>";
+                    foreach ($items as $item) {
+                        if ($item["id_order"] == $order["id_order"]) {
+                            echo "<div class='history-content'>";
+                            echo "<div class='history-img-container'>";
+                            echo "<img class='history-img' src='./imgs/" . $item["dish_img"] . "' alt=''>";
+                            echo "</div>";
+                            echo "<div class='history-text'>";
+                            echo "<div class='histoy-text-container'>";
+                            echo "<p class='title-history-text'>Dish name</p>";
+                            echo "<p>" . $item["dish_name"] . "</p>";
+                            echo "</div>";
+                            echo "<div class='histoy-text-container'>";
+                            echo "<p class='title-history-text'>Quantity</p>";
+                            echo "<p>" . $item["qty"] . "</p>";
+                            echo "</div>";
+                            echo "<div class='histoy-text-container'>";
+                            echo "<p class='title-history-text'>Price</p>";
+                            echo "<p>$" . $item["price"] . "</p>";
+                            echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
+                        }
                     }
+                    echo "</div>";
                 }
-                else{
-                    echo "<h3>empty history</h3>";
-                }
+            } else {
+                echo "<h3>empty history</h3>";
+            }
             ?>
-            
+
             <div class="history-item">
                 <div class="history-header">
                     <p>Date: 00/00/00</p>
@@ -135,7 +140,7 @@
                             <p>name</p>
                             <p>test</p>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -143,8 +148,9 @@
         </section>
     </main>
 
-    <?php 
-        include "./parts/footer.php";
+    <?php
+    include "./parts/footer.php";
     ?>
 </body>
+
 </html>
